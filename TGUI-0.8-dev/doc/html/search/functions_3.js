9c86b778621d82f9508440de4b362e9046282995
johnhoth <s20b1_zharov@179.ru>
2019-02-04 10:22:49 +0300

    zero commit with empty main.cpp of client

---
+++ TGUI-0.8-dev/doc/html/search/functions_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['emit',['emit',['../classtgui_1_1Signal.html#a00dc3f44531dc832327cf6a0487bece8',1,'tgui::Signal']]],
+  ['enabletabkeyusage',['enableTabKeyUsage',['../classtgui_1_1Gui.html#a57a4ab737501f2576777a658c05efb44',1,'tgui::Gui']]]
+];
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Ivan Zharov on 2019-02-04.
+//
+
+#include <TGUI/TGUI.hpp>
+
+
+int main()
+{
+
+
+    return 0;
+}
